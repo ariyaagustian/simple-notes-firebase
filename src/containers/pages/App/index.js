f085924310045ff8f4192f62d5c5171b0fd8e76b
Ariya Agustian <ariya.riyadh@wgs-id.com>
2020-11-10 09:36:33 +0700

    Setup Redux Pada Project - #4 ReactJS-Firebase Tutorial [Indonesia]

--- src/containers/pages/App/index.js
+++ src/containers/pages/App/index.js
@@ -1,24 +1,25 @@
-import logo from "../../../assets/img/logo/logo.svg";
+// import logo from "../../../assets/img/logo/logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Dashboard from "../Dashboard";
+import Register from "../Register";
+import Login from "../Login";
+import { Provider } from "react-redux";
+import { store } from "../../../config/redux";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/" exact component={Dashboard}></Route>
+            <Route path="/register" component={Register}></Route>
+            <Route path="/login" component={Login}></Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
